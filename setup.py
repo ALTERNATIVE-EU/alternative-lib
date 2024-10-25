89bc36f52d4281fff14125c42c56157aef79ca0c
darashevcstbg <72433964+darashevcstbg@users.noreply.github.com>
2024-10-25 18:21:08 +0300

    Fix download file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='alternative-eu-lib',
     packages=find_packages(include=['alternative_eu_lib']),
-    version='0.1.1',
+    version='0.2.0',
     description='Python library for working with the Alternative platform.',
     author='ALTERNATIVE',
     license='MIT',

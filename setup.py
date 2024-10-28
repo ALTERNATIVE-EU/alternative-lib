c207c005936193f2a2b16d053f15dadfc49c6c79
Desislav Arashev <darashev@cst-bg.net>
2024-10-28 11:36:25 +0200

    Fix library name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='alternative-eu-lib',
     packages=find_packages(include=['alternative_lib']),
-    version='0.2.0',
+    version='0.2.1',
     description='Python library for working with the Alternative platform.',
     author='ALTERNATIVE',
     license='MIT',
